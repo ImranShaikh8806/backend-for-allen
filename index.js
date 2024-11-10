d8b9c2b7e19a6fb218d94dd851529638784796a1
ImranShaikh8806 <imranshaikh880672@gmail.com>
2024-11-10 15:47:26 +0530

    updated vercel.json

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
 
+
 const { userRouter } = require("./routes/user");
-const { courseRouter } = require("./routes/courese");  
+const { courseRouter } = require("./routes/courese");
 const { adminRouter } = require("./routes/admin");
-
 const app = express();
-
-// Middleware setup
 app.use(express.json());
 app.use(cors());
 
-// Define routes
+
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/course", courseRouter);
 
-// MongoDB connection and server setup
 async function main() {
-  try {
-    // Connect to MongoDB
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connected to MongoDB");
-
-    // Get the port from the environment or default to 3000
+    await mongoose.connect(process.env.MONGO_URL)
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}`);
     });
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
+    console.log("listening on port 3000")
 }
 
-main();
+main()
