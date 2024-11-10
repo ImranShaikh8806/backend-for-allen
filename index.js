f986cd76877e77d842c08b5169ed0722d82d795c
ImranShaikh8806 <imranshaikh880672@gmail.com>
2024-11-10 15:19:54 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
 
-
 const { userRouter } = require("./routes/user");
-const { courseRouter } = require("./routes/courese");
+const { courseRouter } = require("./routes/courese");  
 const { adminRouter } = require("./routes/admin");
+
 const app = express();
+
+// Middleware setup
 app.use(express.json());
 app.use(cors());
 
-
+// Define routes
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/course", courseRouter);
 
+// MongoDB connection and server setup
 async function main() {
-    await mongoose.connect(process.env.MONGO_URL)
-    app.listen(3000);
-    console.log("listening on port 3000")
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+
+    // Get the port from the environment or default to 3000
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 }
 
-main()
+main();
